80c28be234d03037b814d8bea656b911043614e2
julian-heng <julianhengwl@gmail.com>
2019-08-22 21:13:27 +0800

    sysctl.py: Add check()

--- sys_line/tools/sysctl.py
+++ sys_line/tools/sysctl.py
@@ -6,8 +6,9 @@ from .utils import run
 class Sysctl():
 
     def __init__(self):
+        check = lambda i: i and ":" in i
         self.sysctl = run(["sysctl", "-A"]).strip().split("\n")
-        self.sysctl = dict(i.split(":", 1) for i in self.sysctl if i)
+        self.sysctl = dict(i.split(":", 1) for i in self.sysctl if check(i))
 
 
     def query(self, key):
