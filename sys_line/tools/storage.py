79311fcece4a4e299ae8c8c9410565c958881397
julian-heng <julianhengwl@gmail.com>
2019-08-25 13:26:05 +0800

    storage.py: Use property and setters

--- sys_line/tools/storage.py
+++ sys_line/tools/storage.py
@@ -5,18 +5,19 @@
 
 class Storage():
     """ Storage class for storing values with data prefixes """
-    prefixes = ("B", "KiB", "MiB", "GiB", "TiB", "auto")
+    PREFIXES = ("B", "KiB", "MiB", "GiB", "TiB", "auto")
 
     def __init__(self,
                  value: int = 0,
                  prefix: str = "B",
                  rounding: int = -1) -> None:
-        self.value = value
-        self.prefix = prefix
-        self.rounding = rounding
+        self.__value = value
+        self.__prefix = prefix
+        self.__rounding = rounding
 
 
     def __repr__(self) -> str:
+        # Prevent cyclic import
         from .utils import _round
 
         val = self.value
@@ -31,60 +32,60 @@ class Storage():
 
     def __add__(self, other: object) -> None:
         val, other, is_storage = self.__check_storage(other)
-        self.set_value(val + other)
+        self.value = val + other
 
         if is_storage:
-            tmp_prefix = self.get_prefix()
+            tmp_prefix = self.prefix
             self.set_prefix_without_value("B")
-            self.set_prefix(tmp_prefix)
+            self.prefix = tmp_prefix
 
         return self
 
 
     def __sub__(self, other: object) -> None:
         val, other, is_storage = self.__check_storage(other)
-        self.set_value(val - other)
+        self.value = val - other
 
         if is_storage:
-            tmp_prefix = self.get_prefix()
+            tmp_prefix = self.prefix
             self.set_prefix_without_value("B")
-            self.set_prefix(tmp_prefix)
+            self.prefix = tmp_prefix
 
         return self
 
 
     def __mul__(self, other: object) -> None:
         val, other, is_storage = self.__check_storage(other)
-        self.set_value(val * other)
+        self.value = val * other
 
         if is_storage:
-            tmp_prefix = self.get_prefix()
+            tmp_prefix = self.prefix
             self.set_prefix_without_value("B")
-            self.set_prefix(tmp_prefix)
+            self.prefix = tmp_prefix
 
         return self
 
 
     def __truediv__(self, other: object) -> None:
         val, other, is_storage = self.__check_storage(other)
-        self.set_value(val / other)
+        self.value = val / other
 
         if is_storage:
-            tmp_prefix = self.get_prefix()
+            tmp_prefix = self.prefix
             self.set_prefix_without_value("B")
-            self.set_prefix(tmp_prefix)
+            self.prefix = tmp_prefix
 
         return self
 
 
     def __rtruediv__(self, other: object) -> None:
         val, other, is_storage = self.__check_storage(other)
-        self.set_value(other / val)
+        self.value = other / val
 
         if is_storage:
-            tmp_prefix = self.get_prefix()
+            tmp_prefix = self.prefix
             self.set_prefix_without_value("B")
-            self.set_prefix(tmp_prefix)
+            self.prefix = tmp_prefix
 
         return self
 
@@ -98,48 +99,58 @@ class Storage():
 
 
     def __calc_prefix_delta(self, start: int, end: int) -> int:
-        return self.prefixes.index(end) - self.prefixes.index(start)
+        return self.PREFIXES.index(end) - self.PREFIXES.index(start)
 
 
     def __check_storage(self, other: object) -> (int, int, bool):
         chk = isinstance(other, Storage)
-        return (self.get_bytes() if chk else self.value,
-                other.get_bytes() if chk else other, chk)
+        return (self.bytes if chk else self.value,
+                other.bytes if chk else other, chk)
 
 
-    def set_value(self, value: int) -> None:
-        """ Change the value """
-        self.value = value
+    @property
+    def value(self) -> [int, float]:
+        return self.__value
 
 
-    def set_prefix(self, prefix: str) -> None:
-        """ Change the current prefix to a another prefix """
+    @value.setter
+    def value(self, value: int) -> None:
+        self.__value = value
+
+
+    @property
+    def prefix(self) -> str:
+        return self.__prefix
+
+
+    @prefix.setter
+    def prefix(self, prefix: str) -> None:
         if prefix == "auto":
             count = 0
-            while self.value > 1024:
-                self.value /= 1024
+            while self.__value > 1024:
+                self.__value /= 1024
                 count += 1
-            curr_index = self.prefixes.index(self.prefix) + count
-            self.prefix = self.prefixes[curr_index]
+            curr_index = self.PREFIXES.index(self.__prefix) + count
+            self.__prefix = self.PREFIXES[curr_index]
         else:
             try:
-                delta = self.__calc_prefix_delta(self.prefix, prefix)
+                delta = self.__calc_prefix_delta(self.__prefix, prefix)
                 if delta != 0:
                     # Convert the value
-                    self.value = self.value / pow(1024, delta)
-                    self.prefix = prefix
+                    self.__value = self.__value / pow(1024, delta)
+                    self.__prefix = prefix
             except ValueError:
                 pass
 
 
-    def get_value(self) -> int:
-        """ Return value """
-        return self.value
+    @property
+    def rounding(self) -> int:
+        return self.__rounding
 
 
-    def get_prefix(self) -> str:
-        """ Return prefix """
-        return self.value
+    @rounding.setter
+    def rounding(self, rounding) -> None:
+        self.__rounding = rounding
 
 
     def set_prefix_without_value(self, prefix: str) -> None:
@@ -147,7 +158,8 @@ class Storage():
         self.prefix = prefix
 
 
-    def get_bytes(self) -> int:
+    @property
+    def bytes(self) -> int:
         """ Return storage amount in bytes """
         val = self.value
         delta = self.__calc_prefix_delta(self.prefix, "B")
