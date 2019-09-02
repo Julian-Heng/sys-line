1ae591b4f5e188140a9fec052f257509bd44212a
julian-heng <julianhengwl@gmail.com>
2019-09-02 21:14:53 +0800

    cpu.c: Linux support

--- src/cpu.c
+++ src/cpu.c
@@ -1,12 +1,15 @@
+#if defined(__linux__)
+#   define _DEFAULT_SOURCE
+#   include <regex.h>
+#elif defined(__APPLE__) && defined(__MACH__)
+#   include <sys/types.h>
+#   include <sys/sysctl.h>
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-
-#if defined(__APPLE__) && defined(__MACH__)
-#include <sys/types.h>
-#include <sys/sysctl.h>
-#endif
 
 #include "cpu.h"
 
@@ -37,10 +40,26 @@ bool get_cores(struct cpu_info* cpu)
     bool ret = false;
     int cores = 0;
 
-#if defined(__APPLE__) && defined(__MACH__)
-    size_t len = sizeof(cores);
+#if defined(__linux__)
+    FILE* fp;
+    char buf[BUFSIZ];
+    regex_t re;
 
+    if ((ret = (fp = fopen("/proc/cpuinfo", "r")) &&
+        ! regcomp(&re, "^processor", REG_EXTENDED)))
+    {
+        while (fgets(buf, BUFSIZ, fp))
+            if (! regexec(&re, buf, 0, NULL, 0))
+                cores++;
+
+        fclose(fp);
+        regfree(&re);
+    }
+
+#elif defined(__APPLE__) && defined(__MACH__)
+    size_t len = sizeof(cores);
     ret = ! sysctlbyname("hw.logicalcpu_max", &cores, &len, NULL, 0);
+
 #endif
 
     if (ret)
@@ -54,7 +73,25 @@ bool get_cpu(struct cpu_info* cpu)
 {
     bool ret = false;
 
-#if defined(__APPLE__) && defined(__MACH__)
+#if defined(__linux__)
+    FILE* fp;
+    char buf[BUFSIZ];
+    regex_t re;
+    regmatch_t group[2];
+
+    if ((fp = fopen("/proc/cpuinfo", "r")) &&
+        ! regcomp(&re, "model name\\s+: (.*)", REG_EXTENDED))
+    {
+        while (! ret && fgets(buf, BUFSIZ, fp))
+            if ((ret = ! regexec(&re, buf, 2, group, 0)))
+                strncpy(cpu->cpu, buf + group[1].rm_so,
+                        group[1].rm_eo - group[1].rm_so - 1);
+
+        fclose(fp);
+        regfree(&re);
+    }
+
+#elif defined(__APPLE__) && defined(__MACH__)
     size_t len = sizeof(cpu->cpu);
 
     ret = ! sysctlbyname("machdep.cpu.brand_string", cpu->cpu, &len, NULL, 0);
@@ -68,7 +105,20 @@ bool get_load(struct cpu_info* cpu)
 {
     bool ret = false;
 
-#if defined(__APPLE__) && defined(__MACH__)
+#if defined(__linux__)
+    FILE* fp;
+    char buf[BUFSIZ];
+
+    if ((ret = (fp = fopen("/proc/loadavg", "r")) &&
+                fgets(buf, BUFSIZ, fp)))
+    {
+        sscanf(buf, "%f %f %f", &(cpu->load[0]),
+                                &(cpu->load[1]),
+                                &(cpu->load[2]));
+        fclose(fp);
+    }
+
+#elif defined(__APPLE__) && defined(__MACH__)
     struct loadavg load;
     size_t len = sizeof(load);
 
@@ -88,9 +138,9 @@ bool get_cpu_usage(struct cpu_info* cpu)
     char buf[BUFSIZ];
     float val = 0.0;
 
-    if (cpu->cores == 0)
+    if (! cpu->cores)
         get_cores(cpu);
-    if (cpu->cores == 0)
+    if (cpu->cores)
         return ret;
 
     if ((ret = (ps = popen("ps -e -o %cpu", "r"))))
@@ -113,7 +163,18 @@ bool get_uptime(struct cpu_info* cpu)
 {
     bool ret = false;
 
-#if defined(__APPLE__) && defined(__MACH__)
+#if defined(__linux__)
+    FILE* fp;
+    char buf[BUFSIZ];
+
+    if ((ret = (fp = fopen("/proc/uptime", "r")) &&
+                fgets(buf, BUFSIZ, fp)))
+    {
+        sscanf(buf, "%d", &(cpu->uptime));
+        fclose(fp);
+    }
+
+#elif defined(__APPLE__) && defined(__MACH__)
     struct timeval uptime;
     size_t len = sizeof(uptime);
 
