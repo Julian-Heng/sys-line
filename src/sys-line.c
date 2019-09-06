df1f4ba1c37a550be3b524bd5bd48a028920c426
julian-heng <julianhengwl@gmail.com>
2019-09-06 23:26:39 +0800

    sys-line.c: Change printing method

--- src/sys-line.c
+++ src/sys-line.c
@@ -10,25 +10,30 @@ int main(void)
 
     if ((sys = init_system()))
     {
-        get_cores(sys->cpu);
-        get_cpu(sys->cpu);
-        get_load(sys->cpu);
-        get_cpu_usage(sys->cpu);
-        get_fan(sys->cpu);
-        get_temp(sys->cpu);
-
-        get_uptime(sys->cpu);
-
         printf("sys:\t%p\n", (void*)sys);
         printf("cpu:\t%p\n", (void*)sys->cpu);
+
+        get_cores(sys->cpu);
         printf("cpu.cores:\t%d\n", sys->cpu->cores);
+
+        get_cpu(sys->cpu);
         printf("cpu.cpu:\t%s\n", sys->cpu->cpu);
+
+        get_load(sys->cpu);
         printf("cpu.load:\t%f %f %f\n", sys->cpu->load[0],
                                         sys->cpu->load[1],
                                         sys->cpu->load[2]);
+
+        get_cpu_usage(sys->cpu);
         printf("cpu.cpu_usage:\t%f\n", sys->cpu->cpu_usage);
+
+        get_fan(sys->cpu);
         printf("cpu.fan:\t%d\n", sys->cpu->fan);
+
+        get_temp(sys->cpu);
         printf("cpu.temp:\t%f\n", sys->cpu->temp);
+
+        get_uptime(sys->cpu);
         printf("cpu.uptime:\t%d\n", sys->cpu->uptime);
 
         free(sys->cpu);
