cf13ce9d51b567f652157077e6040eaf291f0e51
julian-heng <julianhengwl@gmail.com>
2019-09-07 16:07:12 +0800

    macros.h: Redo macros

--- src/macros.h
+++ src/macros.h
@@ -1,6 +1,18 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define _free(p) __free((void**)(&(p)))
+#define _free(p) \
+    if ((p)) \
+    { \
+        free((p)); \
+        (p) = NULL; \
+    }
+
+#define _fclose(f) \
+    if ((f)) \
+    { \
+        fclose((f)); \
+        (f) = NULL; \
+    }
 
 #endif
