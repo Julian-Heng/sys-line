feab2ce8d7baf1be9b90fc4b16cba1c1b7504341
julian-heng <julianhengwl@gmail.com>
2019-09-27 16:36:25 +0800

    Linux swap

--- src/systems/linux/swap.c
+++ src/systems/linux/swap.c
@@ -1,6 +1,12 @@
 #if defined(__linux__)
 
+#include <regex.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "../../utils/macros.h"
 
 #include "swap.h"
 
@@ -8,6 +14,39 @@
 bool __get_swap_used(struct swap_info* swap)
 {
     bool ret = false;
+    long long free = 0;
+
+    FILE* fp;
+
+    bool cond = false;
+    char buf[BUFSIZ];
+    char tmp[BUFSIZ];
+
+    regex_t re;
+    regmatch_t group[2];
+
+    if (! swap->total)
+        __get_swap_total(swap);
+
+    if ((ret = (fp = fopen("/proc/meminfo", "r"))) &&
+        ! regcomp(&re, USED_REG, REG_EXTENDED))
+    {
+        memset(buf, 0, BUFSIZ);
+        memset(tmp, 0, BUFSIZ);
+
+        while (! cond && fgets(buf, BUFSIZ, fp))
+            if ((cond = ! regexec(&re, buf, 2, group, 0)))
+                strncpy(tmp, buf + group[1].rm_so,
+                        group[1].rm_eo - group[1].rm_so);
+
+        free = atoll(tmp) << 10;
+        regfree(&re);
+    }
+
+    _fclose(fp);
+
+    if (ret)
+        swap->used = swap->total - free;
 
     return ret;
 }
@@ -16,6 +55,36 @@ bool __get_swap_used(struct swap_info* swap)
 bool __get_swap_total(struct swap_info* swap)
 {
     bool ret = false;
+    long long total = 0;
+
+    FILE* fp;
+
+    bool cond = false;
+    char buf[BUFSIZ];
+    char tmp[BUFSIZ];
+
+    regex_t re;
+    regmatch_t group[2];
+
+    if ((ret = (fp = fopen("/proc/meminfo", "r"))) &&
+        ! regcomp(&re, TOTAL_REG, REG_EXTENDED))
+    {
+        memset(buf, 0, BUFSIZ);
+        memset(tmp, 0, BUFSIZ);
+
+        while (! cond && fgets(buf, BUFSIZ, fp))
+            if ((cond = ! regexec(&re, buf, 2, group, 0)))
+                strncpy(tmp, buf + group[1].rm_so,
+                        group[1].rm_eo - group[1].rm_so);
+
+        total = atoll(tmp) << 10;
+        regfree(&re);
+    }
+
+    _fclose(fp);
+
+    if (ret)
+        swap->total = total;
 
     return ret;
 }
--- src/systems/linux/swap.h
+++ src/systems/linux/swap.h
@@ -4,6 +4,9 @@
 
 #include "../commons/swap.h"
 
+#define TOTAL_REG "^SwapTotal:\\s+([0-9]+)"
+#define USED_REG "^SwapFree:\\s+([0-9]+)"
+
 bool __get_swap_used(struct swap_info*);
 bool __get_swap_total(struct swap_info*);
 
