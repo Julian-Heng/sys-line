3e5f678e6122367f798a7d807aae766698d9a8fd
julian-heng <julianhengwl@gmail.com>
2020-06-25 19:52:32 +0800

    Reorganise project directory

--- setup.py
+++ setup.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python3
 
-""" setup.py """
-
+from pathlib import Path
 import setuptools
+
+
+class CleanCommand(setuptools.Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        def find_files(path, pattern, files):
+            for j in Path(path).glob(pattern):
+                if j.is_dir():
+                    find_files(j, "*", files)
+                files.append(j)
+
+        pattern = ("build", "dist", "*.pyc", "*.tgz", "src/*.egg-info")
+        files = []
+
+        for i in pattern:
+            find_files(".", i, files)
+
+        for i in files:
+            if i.is_dir():
+                print("removed directory '{}'".format(i))
+                i.rmdir()
+            else:
+                print("removed '{}'".format(i))
+                i.unlink()
+
 
 setuptools.setup(
     name="sys-line",
@@ -10,8 +42,10 @@ setuptools.setup(
     author="Julian Heng",
     author_email="julianhengwl@gmail.com",
     description="a simple status line generator",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
     entry_points={
         "console_scripts": ["sys-line = sys_line.__main__:main"]
-    }
+    },
+    cmdclass={"clean": CleanCommand}
 )
