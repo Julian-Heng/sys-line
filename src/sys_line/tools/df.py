b91ebde30d262affdcff5ddef7604e47a82da451
julian-heng <julianhengwl@gmail.com>
2020-06-30 18:57:58 +0800

    df.py: PEP8 compliant

--- src/sys_line/tools/df.py
+++ src/sys_line/tools/df.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+# pylint: disable=too-many-arguments
 
 """ df model class """
 
 
 class DfEntry():
+    """ DfEntry class to store the columns for a line in the output of 'df' """
 
     def __init__(self, filesystem, blocks, used, available, percent, mount):
         self._filesystem = filesystem
@@ -15,24 +17,30 @@ class DfEntry():
 
     @property
     def filesystem(self):
+        """ Returns the value under the 'filesystem' column """
         return self._filesystem
 
     @property
     def blocks(self):
+        """ Returns the value under the 'blocks' column """
         return self._blocks
 
     @property
     def used(self):
+        """ Returns the value under the 'used' column """
         return self._used
 
     @property
     def available(self):
+        """ Returns the value under the 'available' column """
         return self._available
 
     @property
     def percent(self):
+        """ Returns the value under the 'percent' column """
         return self._percent
 
     @property
     def mount(self):
+        """ Returns the value under the 'mount' column """
         return self._mount
