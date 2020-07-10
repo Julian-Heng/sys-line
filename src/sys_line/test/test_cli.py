a792cc24da67aa2c4f22b97c00c85d8014bae82b
julian-heng <julianhengwl@gmail.com>
2020-07-10 19:29:00 +0800

    test: Add test_cli.py

---
+++ src/sys_line/test/test_cli.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from types import SimpleNamespace
+
+from ..tools.cli import flatten, unique, dict_to_namespace
+
+
+class TestFlatten(unittest.TestCase):
+
+    def test__flatten(self):
+        self.assertEqual(flatten(["a"]), ["a"])
+        self.assertEqual(flatten(["a", ["a"]]), ["a", "a"])
+
+
+class TestUnique(unittest.TestCase):
+
+    def test__unique(self):
+        self.assertEqual(unique([""]), [""])
+        self.assertEqual(unique(["a"]), ["a"])
+        self.assertEqual(unique(["a", "b"]), ["a", "b"])
+        self.assertEqual(unique(["a", "b", "a"]), ["a", "b"])
+
+
+class TestDictToNamespace(unittest.TestCase):
+
+    def test__dict_to_namespace(self):
+        inputs = [
+            {},
+            {"a": "b"},
+            {"a": {"b": "c"}},
+            {"x": {"a": {"b": "c"}, "d": ["e", "f"]}, "y": ("z")},
+        ]
+
+        for i in inputs:
+            self._validate(dict_to_namespace(i), i)
+
+    def _validate(self, nspace, attr_dict):
+        self.assertEqual(isinstance(nspace, SimpleNamespace), True)
+        for key in attr_dict.keys():
+            self.assertEqual(hasattr(nspace, key), True)
+            value = getattr(nspace, key)
+            if isinstance(value, SimpleNamespace):
+                self._validate(value, attr_dict[key])
+            else:
+                self.assertEqual(value, attr_dict[key])
