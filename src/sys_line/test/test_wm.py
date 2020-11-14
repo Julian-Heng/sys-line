817015c9ec8bf235728b786c1b472f80079c9e06
julian-heng <julianhengwl@gmail.com>
2020-11-15 00:34:12 +0800

    test: Add test_wm.py

---
+++ src/sys_line/test/test_wm.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from unittest.mock import MagicMock, PropertyMock, patch
+
+from ..systems.wm import Yabai, WindowManagerStub
+
+
+class TestYabai(unittest.TestCase):
+
+    def setUp(self):
+        super(TestYabai, self).setUp()
+
+        self.wm = Yabai("wm", None)
+        self.which_patch = patch("shutil.which").start()
+        self.run_patch = patch("sys_line.systems.wm.run").start()
+
+        self.yabai_exe = "/usr/local/bin/yabai"
+        self.query_spaces_out = ("""
+{
+	"id":3,
+	"label":"",
+	"index":1,
+	"display":1,
+	"windows":[52, 1263],
+	"type":"bsp",
+	"visible":1,
+	"focused":1,
+	"native-fullscreen":0,
+	"first-window":52,
+	"last-window":52
+}
+""")
+
+        self.query_windows_out = ("""
+{
+	"id":52,
+	"pid":543,
+	"app":"iTerm2",
+	"title":"[0] bash: Julians-MBP",
+	"frame":{
+		"x":4.0000,
+		"y":4.0000,
+		"w":1672.0000,
+		"h":1022.0000
+	},
+	"level":0,
+	"role":"AXWindow",
+	"subrole":"AXStandardWindow",
+	"movable":1,
+	"resizable":1,
+	"display":1,
+	"space":1,
+	"visible":1,
+	"focused":1,
+	"split":"none",
+	"floating":0,
+	"sticky":0,
+	"minimized":0,
+	"topmost":0,
+	"opacity":1.0000,
+	"shadow":0,
+	"border":1,
+	"stack-index":0,
+	"zoom-parent":0,
+	"zoom-fullscreen":0,
+	"native-fullscreen":0
+}
+""")
+
+    def tearDown(self):
+        super(TestYabai, self).tearDown()
+        patch.stopall()
+
+    def test__yabai_desktop_index(self):
+        self.which_patch.return_value = self.yabai_exe
+        self.run_patch.return_value = self.query_spaces_out
+
+        self.assertEqual(self.wm.desktop_index, 1)
+
+        args, _ = self.which_patch.call_args
+        self.assertEqual(args, ("yabai",))
+
+        args, _ = self.run_patch.call_args
+        expected = [
+            self.which_patch.return_value, "-m", "query", "--spaces", "--space"
+        ]
+        self.assertEqual(args, (expected,))
+
+    def test__yabai_desktop_name(self):
+        self.which_patch.return_value = self.yabai_exe
+        self.run_patch.return_value = self.query_spaces_out
+
+        self.assertEqual(self.wm.desktop_name, "Desktop 1")
+
+        args, _ = self.which_patch.call_args
+        self.assertEqual(args, ("yabai",))
+
+        args, _ = self.run_patch.call_args
+        expected = [
+            self.which_patch.return_value, "-m", "query", "--spaces", "--space"
+        ]
+        self.assertEqual(args, (expected,))
+
+    def test__yabai_app_name(self):
+        self.which_patch.return_value = self.yabai_exe
+        self.run_patch.return_value = self.query_windows_out
+
+        self.assertEqual(self.wm.app_name, "iTerm2")
+
+        args, _ = self.which_patch.call_args
+        self.assertEqual(args, ("yabai",))
+
+        args, _ = self.run_patch.call_args
+        expected = [
+            self.which_patch.return_value, "-m", "query", "--windows",
+            "--window"
+        ]
+        self.assertEqual(args, (expected,))
+
+    def test__yabai_window_name(self):
+        self.which_patch.return_value = self.yabai_exe
+        self.run_patch.return_value = self.query_windows_out
+
+        self.assertEqual(self.wm.window_name, "[0] bash: Julians-MBP")
+
+        args, _ = self.which_patch.call_args
+        self.assertEqual(args, ("yabai",))
+
+        args, _ = self.run_patch.call_args
+        expected = [
+            self.which_patch.return_value, "-m", "query", "--windows",
+            "--window"
+        ]
+        self.assertEqual(args, (expected,))
